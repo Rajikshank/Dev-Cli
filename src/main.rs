b1b09449f44f38c84e57fcca2e45711cc0b2f253
Rajikshank <krajikshan637@gmail.com>
2026-06-05 23:45:07 +0530

    strcuct change

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 use clap::{Parser,Subcommand};
 
 
+#[derive(Debug)]
+struct Bookmark{
+    url:String,
+    tags:Vec<String>,
+}
+
+
 #[derive(Parser)]
 #[command(name="devlink")]
 #[command(version="0.1.0")]
@@ -18,6 +25,10 @@ enum Commands{
     
     Add{
         url:String,
+
+        #[arg(short,long="tag")]
+          tags:Vec<String>,  
+
     },
 
     List,
@@ -33,8 +44,23 @@ fn main() {
   let cli =Cli::parse();
 
   match cli.command{
-    Commands::Add {url}=>{
-        println!("Adding bookmark: {}",url);
+    Commands::Add {url,tags}=>{
+        let bookmark= Bookmark{url,tags};
+
+        println!("New bookmark created:");
+       println!("{:#?}",bookmark);
+        
+        if bookmark.tags.is_empty(){
+          println!("No bookmar added");
+        }
+
+        else{
+            println!("Tags:");
+
+            for tag in &bookmark.tags{
+                println!("-{}",tag);
+            }
+        }
     }
     Commands::List=>{
         println!("Listing all bookmarks...");
